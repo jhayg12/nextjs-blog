c5088708c4cdf8bc583ffc0a83874e20698d17a8
Jayson Suyat <jayson.suyat@puttiapps.com>
2023-08-24 08:26:52 +0800

    .

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I’m Shu. I’m a software engineer and a translator (English/Japanese). You can contact me on</p>
+        <p>Hello, I’m Jayson. I’m a software engineer at PuttiApps. You can contact me on skype and teamwork chat</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
